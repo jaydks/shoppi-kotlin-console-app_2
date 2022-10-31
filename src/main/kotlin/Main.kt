9aa0f7f4c1e442232d54551d041c019cb07b38f8
jaydks <jjeee1313@gmail.com>
2022-11-01 01:21:10 +0900

    실습 3-3. Product 객체 추가 및 ProductList 화면이 상품 출력 담당

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
 import screen.ShoppingHome
 
-/*
-Step1. main 함수 -> 클래스이 함수
-Step2. 화면별 클래스 분리
-Step3. 선택한 카테고리 상품 목록 표시
-
- */
-
-
 fun main(args: Array<String>){
     val shoppingHome = ShoppingHome()
     shoppingHome.start()
